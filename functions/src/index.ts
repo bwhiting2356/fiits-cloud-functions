f763b102115db49a835dc4ddf4eabb124fed0b97
bwhiting2356 <brendan.whiting@me.com>
2019-11-29 23:37:20 -0800

    [#170009065] send post message to create user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import axios from 'axios';
+
+admin.initializeApp(functions.config().firebase);
+
+const ref = admin.database().ref();
+
 import { fetchAndSaveToBigQuery } from './fetchData';
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+
 export const getBikeShareData = functions.pubsub
     .topic('get-bikeshare-data')
     .onPublish(() => {
@@ -10,4 +15,7 @@ export const getBikeShareData = functions.pubsub
     });
 
 
-export const newUser = functions.auth.user().onCreate
+export const newUser = functions.auth.user().onCreate(event => {
+    const { photoURL, uid, email } = event;
+    return axios.post('https://fiits-backend.herokuapp.com', { id: uid, email, photoURL, balance: 10.00 })
+})
