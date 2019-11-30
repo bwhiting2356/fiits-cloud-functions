c6a2fe0e547ee4746324022e320f12dcba87c179
bwhiting2356 <brendan.whiting@me.com>
2019-11-30 14:25:53 -0800

    [#170033812] remove balance from cloud function

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -3,13 +3,12 @@ import axios from 'axios';
 
 interface UserAccount {
     id: string;
-    balance: number;
     email: string | undefined;
     photoURL: string | undefined;
 }
 
 export const createUser = (event: auth.UserRecord) => {
     const { photoURL, uid, email } = event;
-    const newUser: UserAccount = { id: uid, email, photoURL, balance: 10.00 }
+    const newUser: UserAccount = { id: uid, email, photoURL }
     return axios.post('https://fiits-backend.herokuapp.com/create-user', newUser)
 }
