00ed3217832cb65c85252e36aa738c2c745d552d
bwhiting2356 <brendan.whiting@me.com>
2019-12-11 15:04:16 -0800

    [#170243905] remove photo url

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -4,12 +4,11 @@ import axios from 'axios';
 interface UserAccount {
     id: string;
     email: string | undefined;
-    photoURL: string | undefined;
 }
 
 export const createUser = (event: auth.UserRecord) => {
-    const { photoURL, uid, email } = event;
-    const newUser: UserAccount = { id: uid, email, photoURL }
+    const { uid, email } = event;
+    const newUser: UserAccount = { id: uid, email }
     return axios.post('https://fiits-backend.herokuapp.com/create-user', newUser)
         .then(() => Promise.resolve)
 }
